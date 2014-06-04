64f8d0ed235ae1692b4f83946745881664a6caad
Matt Edelman <medelman@paypal.com>
2014-06-04 16:50:28 -0700

    updating tests to use active URL, changing to two spaces as tab

--- index.js
+++ index.js
@@ -15,10 +15,10 @@
 'use strict';
 
 var async = require('async'),
-	Setup = require('./setup'),
-	nemoData = {},
-	_ = require('lodash'),
-	webdriver = require('selenium-webdriver');
+  Setup = require('./setup'),
+  nemoData = {},
+  _ = require('lodash'),
+  webdriver = require('selenium-webdriver');
 
 /**
  * Represents a Nemo instance
@@ -29,118 +29,120 @@ var async = require('async'),
 
 function Nemo(config) {
 
-	//console.log('Nemo constructor config: ', config);
-	this.plugins = {};
-	//config is for registering plugins
-	if (config && config.plugins) {
-		this.plugins = config.plugins;
-	}
+  //console.log('Nemo constructor config: ', config);
+  this.plugins = {};
+  //config is for registering plugins
+  if (config && config.plugins) {
+    this.plugins = config.plugins;
+  }
 }
 
 Nemo.prototype = {
-	/**
-	 *
-	 * Nemo.setup
-	 *@param {Object} config -
-	 *    {
-	 *		'view': ['example-login', 'serviceError'],	//optional
-	 *		'locator': ['wallet'],						//optional
-	 *	}
-	 *@returns webdriver.promise - successful fulfillment will return an {Object} as below:
-	 *    {
-	 *		'view': {},		//view instances if specified in config
-	 *		'locator': {},	//locator instances if specified in config
-	 *		'driver': {},	//driver instance. ALWAYS
-	 *		'wd': {},		//static reference to selenium-webdriver. ALWAYS
-	 *	}
-	 */
-	setup: function (config) {
-		nemoData = JSON.parse(process.env.nemoData);
-		config = config || {};
-		var that = this,
-			returnObj = {
-				'view': {},
-				'locator': {},
-				'driver': {},
-				'wd': {}
-			},
-			d = webdriver.promise.defer(),
-			waterFallArray = [driversetup];
+  /**
+   *
+   * Nemo.setup
+   *@param {Object} config -
+   *  {
+   *    'view': ['example-login', 'serviceError']   //optional
+   *    ,'locator': ['wallet']                      //optional
+   *    ,<plugin config namespace>: <plugin config> //optional, depends on plugin setup
+   *  }
+   *@returns webdriver.promise - successful fulfillment will return an {Object} as below:
+   *  {
+   *    'view': {}                           //view instances if specified in config
+   *    ,'locator': {}                       //locator instances if specified in config
+   *    ,'driver': {}                        //driver instance. ALWAYS
+   *    ,'wd': {}                            //static reference to selenium-webdriver. ALWAYS
+   *    ,<plugin namespace>: <plugin object> //if plugin registers
+   *  }
+   */
+  setup: function(config) {
+    nemoData = JSON.parse(process.env.nemoData);
+    config = config || {};
+    var that = this,
+      returnObj = {
+        'view': {},
+        'locator': {},
+        'driver': {},
+        'wd': {}
+      },
+      d = webdriver.promise.defer(),
+      waterFallArray = [driversetup];
 
-		Object.keys(that.plugins).forEach(function (key) {
-			var modulePath,
-				pluginConfig,
-				pluginModule;
+    Object.keys(that.plugins).forEach(function(key) {
+      var modulePath,
+        pluginConfig,
+        pluginModule;
 
-			if ((that.plugins[key].register || config[key]) && key !== 'view') {
-				//register this plugin
-				pluginConfig = that.plugins[key];
-				modulePath = pluginConfig.module;
-				pluginModule = require(modulePath);
-				waterFallArray.push(pluginModule.setup);
-			}
-		});
-		if (config.view) {
-			waterFallArray.push(viewsetup);
-		}
-		if (config.locator) {
-			waterFallArray.push(locatorsetup);
-		}
-		async.waterfall(waterFallArray, function (err, result) {
-			//console.log('waterfall result: ', result);
-			if (err) {
-				d.reject(err);
-			} else {
-				d.fulfill(returnObj);
-			}
-		});
-		return d;
+      if ((that.plugins[key].register || config[key]) && key !== 'view') {
+        //register this plugin
+        pluginConfig = that.plugins[key];
+        modulePath = pluginConfig.module;
+        pluginModule = require(modulePath);
+        waterFallArray.push(pluginModule.setup);
+      }
+    });
+    if (config.view) {
+      waterFallArray.push(viewsetup);
+    }
+    if (config.locator) {
+      waterFallArray.push(locatorsetup);
+    }
+    async.waterfall(waterFallArray, function(err, result) {
+      //console.log('waterfall result: ', result);
+      if (err) {
+        d.reject(err);
+      } else {
+        d.fulfill(returnObj);
+      }
+    });
+    return d;
 
-		//waterfall functions
+    //waterfall functions
 
-		function driversetup(callback) {
-			var toCamelCase = function (match, group1) {
-				return group1.toUpperCase();
-			};
-			//do driver/view/locator/vars setup
-			(new Setup()).doSetup(webdriver, function (err, result) {
-				if (err) {
-					callback(err);
-				} else {
-					//set driver
-					returnObj.driver = result.driver;
-					returnObj.wd = webdriver;
-					returnObj.props = nemoData;
-					callback(null, config, returnObj);
-				}
-			});
-		}
+    function driversetup(callback) {
+      var toCamelCase = function(match, group1) {
+        return group1.toUpperCase();
+      };
+      //do driver/view/locator/vars setup
+      (new Setup()).doSetup(webdriver, function(err, result) {
+        if (err) {
+          callback(err);
+        } else {
+          //set driver
+          returnObj.driver = result.driver;
+          returnObj.wd = webdriver;
+          returnObj.props = nemoData;
+          callback(null, config, returnObj);
+        }
+      });
+    }
 
-		function locatorsetup(config, result, callback) {
-			//setup locators
-			config.locator.forEach(function (key) {
-				returnObj.locator[key] = require(returnObj.props.autoBaseDir + '/locator/' + key);
-			});
-			callback(null, config, returnObj);
-		}
+    function locatorsetup(config, result, callback) {
+      //setup locators
+      config.locator.forEach(function(key) {
+        returnObj.locator[key] = require(returnObj.props.autoBaseDir + '/locator/' + key);
+      });
+      callback(null, config, returnObj);
+    }
 
-		function viewsetup(config, result, callback) {
-			var viewConfig = result;
-			//setup views
-			config.view.forEach(function (key) {
-				//console.log('key is: ' + key)
-				if (that.plugins.view) {
-					//process with the view interface
-					returnObj.view[(key.constructor === String) ? key : key.name] = require(that.plugins.view.module).addView(key, result);
-				} else {
-					var viewMod = require(returnObj.props.autoBaseDir + '/view/' + key);
-					returnObj.view[key] = new viewMod(viewConfig);
-				}
+    function viewsetup(config, result, callback) {
+      var viewConfig = result;
+      //setup views
+      config.view.forEach(function(key) {
+        //console.log('key is: ' + key)
+        if (that.plugins.view) {
+          //process with the view interface
+          returnObj.view[(key.constructor === String) ? key : key.name] = require(that.plugins.view.module).addView(key, result);
+        } else {
+          var viewMod = require(returnObj.props.autoBaseDir + '/view/' + key);
+          returnObj.view[key] = new viewMod(viewConfig);
+        }
 
-			});
-			callback(null, config, returnObj);
-		}
-	}
+      });
+      callback(null, config, returnObj);
+    }
+  }
 };
 
 module.exports = Nemo;
--- setup/index.js
+++ setup/index.js
@@ -15,75 +15,75 @@
 /* global require,module */
 'use strict';
 var fs = require('fs'),
-	webdriver = require('selenium-webdriver'),
-	SeleniumServer = require('selenium-webdriver/remote').SeleniumServer,
-	nemoData = {},
-	driver;
+  webdriver = require('selenium-webdriver'),
+  SeleniumServer = require('selenium-webdriver/remote').SeleniumServer,
+  nemoData = {},
+  driver;
 var Setup = function() {
-	//constructor
+  //constructor
 };
 Setup.prototype = {
-	doSetup: function(_wd, callback) {
-		//look for nemoData env variable, error out if missing
-		nemoData = JSON.parse(process.env.nemoData || {});
-		if (nemoData === {} || nemoData.targetBrowser === undefined) {
-			callback(new Error('[Nemo::doSetup] The nemoData environment variable is missing or not fully defined!'));
-			return;
-		}
-		var caps,
-			tgtBrowser = nemoData.targetBrowser,
-			customCaps = nemoData.serverCaps,
-			serverUrl = nemoData.targetServer,
-			serverProps = nemoData.serverProps,
-			serverJar = nemoData.seleniumJar,
-			errorObject = null;
+  doSetup: function(_wd, callback) {
+    //look for nemoData env variable, error out if missing
+    nemoData = JSON.parse(process.env.nemoData || {});
+    if (nemoData === {} || nemoData.targetBrowser === undefined) {
+      callback(new Error('[Nemo::doSetup] The nemoData environment variable is missing or not fully defined!'));
+      return;
+    }
+    var caps,
+      tgtBrowser = nemoData.targetBrowser,
+      customCaps = nemoData.serverCaps,
+      serverUrl = nemoData.targetServer,
+      serverProps = nemoData.serverProps,
+      serverJar = nemoData.seleniumJar,
+      errorObject = null;
 
-		function getServer() {
-			if (serverProps && (serverUrl.indexOf('127.0.0.1') !== -1 || serverUrl.indexOf('localhost') !== -1)) {
-				//chrome and phantomjs are supported natively. i.e. no webdriver required. chromedriver or phantomjs executables must be in PATH though
-				if (tgtBrowser !== 'chrome' && tgtBrowser !== 'phantomjs') {
-					//make sure there is a jar file
-					var jarExists = fs.existsSync(serverJar);
-					if (!jarExists) {
-						throw new Error('You must specify a valid SELENIUM_JAR value. The value must point to a driver executable in your file system.');
-					}
-					var server = new SeleniumServer(serverJar, serverProps);
-					server.start();
-					serverUrl = server.address();
-				} else {
-					serverUrl = null;
-				}
-			}
-			return serverUrl;
-		}
+    function getServer() {
+      if (serverProps && (serverUrl.indexOf('127.0.0.1') !== -1 || serverUrl.indexOf('localhost') !== -1)) {
+        //chrome and phantomjs are supported natively. i.e. no webdriver required. chromedriver or phantomjs executables must be in PATH though
+        if (tgtBrowser !== 'chrome' && tgtBrowser !== 'phantomjs') {
+          //make sure there is a jar file
+          var jarExists = fs.existsSync(serverJar);
+          if (!jarExists) {
+            throw new Error('You must specify a valid SELENIUM_JAR value. The value must point to a driver executable in your file system.');
+          }
+          var server = new SeleniumServer(serverJar, serverProps);
+          server.start();
+          serverUrl = server.address();
+        } else {
+          serverUrl = null;
+        }
+      }
+      return serverUrl;
+    }
 
-		function getCapabilities() {
-			//exception handling
-			if (!webdriver.Capabilities[tgtBrowser]) {
-				throw new TypeError('You have specified ' + tgtBrowser + ' which is an invalid browser option');
-			}
-			caps = new webdriver.Capabilities();
+    function getCapabilities() {
+      //exception handling
+      if (!webdriver.Capabilities[tgtBrowser]) {
+        throw new TypeError('You have specified ' + tgtBrowser + ' which is an invalid browser option');
+      }
+      caps = new webdriver.Capabilities();
 
-			if (customCaps) {
-				Object.keys(customCaps).forEach(function(key) {
-					caps.set(key, customCaps[key]);
-				});
-			}
-			caps.merge(webdriver.Capabilities[tgtBrowser]());
-			return caps;
-		}
+      if (customCaps) {
+        Object.keys(customCaps).forEach(function(key) {
+          caps.set(key, customCaps[key]);
+        });
+      }
+      caps.merge(webdriver.Capabilities[tgtBrowser]());
+      return caps;
+    }
 
 
-		try {
-			driver = new _wd.Builder().
-			usingServer(getServer()).
-			withCapabilities(getCapabilities()).build();
-		} catch (err) {
-			errorObject = err;
-		}
-		callback(errorObject, {
-			'driver': driver
-		});
-	}
+    try {
+      driver = new _wd.Builder().
+      usingServer(getServer()).
+      withCapabilities(getCapabilities()).build();
+    } catch (err) {
+      errorObject = err;
+    }
+    callback(errorObject, {
+      'driver': driver
+    });
+  }
 };
 module.exports = Setup;
