76362d739c1678b8dc9d1ad35e5439b3671522c6
Nilesh Kulkarni <nikulkarni@paypal.com>
2015-12-16 15:52:37 -0500

    fixed jshint errors and removed results

--- setup/seleniumInstall.js
+++ setup/seleniumInstall.js
@@ -6,27 +6,31 @@ var npmi = require('npmi'),
   error = debug('nemo:error');
 
 var seleniumInstall = function (version) {
- return function(callback) {
-   var options = {
-     name: 'selenium-webdriver',
-     version: version,       // expected version [default: 'latest']
-     path: '.',
-     forceInstall: false,
-     npmLoad: {
-       loglevel: 'silent'
-     }
-   };
-   npmi(options, function (err, result) {
-     if (err) {
-       if (err.code === npmi.LOAD_ERR)    error('npm load error');
-       else if (err.code === npmi.INSTALL_ERR) error('npm install error');
-       callback(err);
-     }
-     // installed
-     log(options.name + '@' + options.version + ' installed successfully in ' + path.resolve(options.path));
-     callback(null);
-   });
- }
+  return function (callback) {
+    var options = {
+      name: 'selenium-webdriver',
+      version: version,       // expected version [default: 'latest']
+      path: '.',
+      forceInstall: false,
+      npmLoad: {
+        loglevel: 'silent'
+      }
+    };
+    npmi(options, function (err) {
+      if (err) {
+        if (err.code === npmi.LOAD_ERR) {
+          error('npm load error');
+        }
+        else if (err.code === npmi.INSTALL_ERR) {
+          error('npm install error');
+        }
+        callback(err);
+      }
+      // installed
+      log(options.name + '@' + options.version + ' installed successfully in ' + path.resolve(options.path));
+      callback(null);
+    });
+  };
 
 
 };
