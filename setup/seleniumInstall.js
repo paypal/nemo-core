a26f1c9110f17aa569caea47d8b3540d4521fa3e
Matt Edelman <medelman@paypal.com>
2016-04-06 12:33:53 -0700

    change to modify package.json to avoid repeated npm installs

--- setup/seleniumInstall.js
+++ setup/seleniumInstall.js
@@ -3,31 +3,36 @@ var path = require('path'),
   debug = require('debug'),
   log = debug('nemo:log'),
   error = debug('nemo:error'),
+  fs = require('fs'),
   exec = require('child_process').exec;
 
 var seleniumInstall = function (version) {
   return function (callback) {
-    var options = {
-      name: 'selenium-webdriver',
-      version: version,       // expected version [default: 'latest']
-      path: '.',
-      forceInstall: false,
-      npmLoad: {
-        loglevel: 'silent'
+    //check package.json
+    var pkg = require(path.resolve(__dirname, '../package.json'));
+    if (pkg.dependencies['selenium-webdriver'] === version) {
+      //already installed
+      return callback(null);
+    }
+    pkg.dependencies['selenium-webdriver'] = version;
+    fs.writeFile(path.resolve(__dirname, '../package.json'), JSON.stringify(pkg, null, '  '), function (err) {
+      if (err) {
+        return error('seleniumInstall: error writing package.json', err);
       }
-    };
-    var cmd = 'npm install ' + options.name + '@' + options.version;
-    log('install cmd is', cmd);
-    exec(cmd, {cwd: path.resolve(__dirname, '..')},
-      function (err, stdout, stderr) {
-        log('stdout', stdout);
-        error('stderr', stderr);
-        if (err !== null) {
-          error('exec error', err);
-          return callback(err);
-        }
-        callback(null);
-      });
+      exec(cmd, {cwd: path.resolve(__dirname, '..')},
+        function (err, stdout, stderr) {
+          log('stdout', stdout);
+          error('stderr', stderr);
+          if (err !== null) {
+            error('exec error', err);
+            return callback(err);
+          }
+          callback(null);
+        });
+    });
+    var cmd = 'npm install';
+
+
   };
 
 
