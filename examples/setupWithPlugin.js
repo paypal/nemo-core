46a08e8582af14397d3976defe32bb5465df2bf5
nikulkarni <nikulkarni@paypal.com>
2016-11-01 11:59:26 -0700

    browserName from capabilities fixed
    console log removed
    
    backward compatible
    
    addressed pr comments
    
    addressed pr comments
    
    console.log updated

--- examples/setupWithPlugin.js
+++ examples/setupWithPlugin.js
@@ -6,9 +6,9 @@ Nemo(basedir, function (err, nemo) {
   if (!!err) {
     console.log('Error during Nemo setup', err);
   }
-  nemo.driver.getCapabilities().
-    then(function (caps) {
-      console.info("Nemo successfully launched", caps.caps_.browserName);
+    nemo.driver.getCapabilities().then(function (caps) {
+        var browserName = caps.get && caps.get('browserName') || caps.caps_.browserName;
+        console.info("Nemo successfully launched", browserName);
     });
   nemo.driver.get(nemo.data.baseUrl);
   nemo.cookie.deleteAll();
